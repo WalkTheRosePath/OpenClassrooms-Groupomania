b5ca4b09a08b4e81b4de0fe160d7353b4e74fe15
Jessica Williams <walktherosepath@gmail.com>
2024-04-23 17:28:14 -0600

    fixed bug running backend

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -4,17 +4,12 @@
 // Import Express and create a router instance
 const express = require('express');
 const router = express.Router();
-
-// Import user controller
 const { userController } = require("../controllers");
-
-// Import middleware
 const authMiddleware = require("../middleware/authMiddleware");
 
 // Define HTTP routes
 router.post("/signup", userController.signup);
 router.post("/login", userController.login);
-router.delete("/user", authMiddleware, userController.deleteUser);
 
 // Export router
 module.exports = router;
