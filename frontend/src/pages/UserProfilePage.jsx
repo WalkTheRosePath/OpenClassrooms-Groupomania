71af5c5cf0dc4ca3e6fb1643c2f8da083236bbfb
Jessica Williams <walktherosepath@gmail.com>
2024-04-24 10:26:29 -0600

    tried debugging delete profile

--- frontend/src/pages/UserProfilePage.jsx
+++ frontend/src/pages/UserProfilePage.jsx
@@ -1,13 +1,16 @@
+// frontend/src/pages/UserProfilePage.jsx
 // Client-side component for the user profile page
 
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import { Navigate } from "react-router-dom";
 import Header from "../components/Header";
 import NavBar from "../components/NavBar";
 import Footer from "../components/Footer";
 
 const UserProfilePage = () => {
   const [user, setUser] = useState(null);
+  const [redirect, setRedirect] = useState(false);
 
   useEffect(() => {
     const fetchUserData = async () => {
@@ -25,6 +28,20 @@ const UserProfilePage = () => {
     fetchUserData();
   }, []);
 
+  const handleDeleteProfile = async () => {
+    try {
+      // Send a DELETE request to the backend API
+      await axios.delete("http://localhost:3000/api/auth/profile");
+      setRedirect(true);
+    } catch (error) {
+      console.error("Error deleting user profile:", error);
+    }
+  };
+
+  if (redirect) {
+    return <Navigate to="/signup" replace={true} />;
+  }
+
   if (!user) {
     return <div>Loading...</div>;
   }
@@ -39,12 +56,11 @@ const UserProfilePage = () => {
           Name: {user.firstName} {user.lastName}
         </p>
         <p>Email: {user.email}</p>
+        <button onClick={handleDeleteProfile}>Delete Profile</button> 
         <Footer />
       </main>
     </div>
   );
 };
-
-// TODO Add delete user button
 
 export default UserProfilePage;
