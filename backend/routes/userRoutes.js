210652e86ffedeafa7ea21872474c8a9165b2ab8
Jessica Williams <walktherosepath@gmail.com>
2024-04-24 19:48:04 -0600

    fixed bug in name of user controller crashing backend

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -4,17 +4,17 @@
 // Import Express and create a router instance
 const express = require('express');
 const router = express.Router();
-const { UserController } = require("../controllers");
+const { userController } = require("../controllers");
 const authMiddleware = require("../middleware/authMiddleware");
 
 // Define HTTP routes for user authentication
-router.post("/signup", UserController.signup);
-router.post("/login", UserController.login);
+router.post("/signup", userController.signup);
+router.post("/login", userController.login);
 
 // Define HTTP routes for user profile
-router.get("/profile/:id", authMiddleware, UserController.getUserProfileById);
-router.put("/profile/:id", authMiddleware, UserController.updateUserProfileById);
-router.delete("/profile/:id", authMiddleware, UserController.deleteUserProfileById);
+router.get("/profile/:id", authMiddleware, userController.getUserProfileById);
+router.put("/profile/:id", authMiddleware, userController.updateUserProfileById);
+router.delete("/profile/:id", authMiddleware, userController.deleteUserProfileById);
 
 // Export router
 module.exports = router;
