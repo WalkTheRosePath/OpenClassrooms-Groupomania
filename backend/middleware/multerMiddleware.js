6460eb0b7e5b886ecd4e88e157fb1d83099a1dae
Jessica Williams <walktherosepath@gmail.com>
2024-04-26 11:24:31 -0600

    updated multer middleware

--- backend/middleware/multerMiddleware.js
+++ backend/middleware/multerMiddleware.js
@@ -15,20 +15,20 @@ const mimeTypes = {
 
 // Configure storage settings for Multer
 const storage = multer.diskStorage({
-  // Specify destination directory for storing files
-  destination: (req, file, callback) => {
-    callback(null, "images");
-  },
-  // Define filename format for uploaded files
-  filename: (req, file, callback) => {
-    // Replace spaces with underscores in the filename
-    const name = file.originalname.split(" ").join("_");
-    // Get file extension from MIME type
-    const extension = mimeTypes[file.mimetype];
-    // Set filename with unique timestamp to avoid overwriting existing files
-    callback(null, name + Date.now() + "." + extension);
-  },
+    // Specify destination directory for storing files
+    destination: (req, file, callback) => {
+        callback(null, 'images');
+    },
+    // Define filename format for uploaded files
+    filename: (req, file, callback) => {
+        // Replace spaces with underscores in the filename
+        const name = file.originalname.split(' ').join('_');
+        // Get file extension from MIME type
+        const extension = mimeTypes[file.mimetype];
+        // Set filename with unique timestamp to avoid overwriting existing files
+        callback(null, name + Date.now() + '.' + extension);
+    }
 });
 
 // Export Multer middleware configured with storage settings for single file uploads
-module.exports = multer({ storage: storage }).single("image");
+module.exports = multer({ storage: storage }).single('image');
