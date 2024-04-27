a5c0a0e10f96b9a35fb002597000f8df096a3f77
Jessica Williams <walktherosepath@gmail.com>
2024-04-26 19:15:42 -0600

    updated comments

--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -8,9 +8,11 @@ const { postController } = require("../controllers");
 const authMiddleware = require("../middleware/authMiddleware");
 const multerMiddleware = require("../middleware/multerMiddleware");
 
-// Define HTTP routes for posts
+// Define HTTP routes for post retrieval
 router.get("/", authMiddleware, postController.getAllPosts);
 router.get("/:id", authMiddleware, postController.getPostById);
+
+// Define HTTP routes for post creation, update, and deletion
 router.post("/", authMiddleware, multerMiddleware, postController.createPost);
 router.put("/:id", authMiddleware, postController.updatePostById);
 router.delete("/:id", authMiddleware, postController.deletePostById);
