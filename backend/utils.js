a88cbf9c0b1dfc280224ddd642afd592dba3b4fd
Jessica Williams <walktherosepath@gmail.com>
2024-05-08 17:27:53 -0600

    deleted unnecessary utils file

--- backend/utils.js
+++
@@ -1,6 +0,0 @@
-// backend/utils.js
-// Functions that can be used in multiple files
-
-exports.createImageUrl = (req) => {
-    return req.protocol + '://' + req.get('host') + '/images/' + req.file.filename
-}
