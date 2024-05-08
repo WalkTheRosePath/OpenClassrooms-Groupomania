b7f2bbe96826ba7e8f109b6b365cec9af117ce41
Jessica Williams <walktherosepath@gmail.com>
2024-05-08 17:26:01 -0600

    deleted unrequired post routes

--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -8,14 +8,10 @@ const { postController } = require("../controllers");
 const authMiddleware = require("../middleware/authMiddleware");
 const multerMiddleware = require("../middleware/multerMiddleware");
 
-// Define HTTP routes for post retrieval
+// Define HTTP routes for posts
 router.get("/", authMiddleware, postController.getAllPosts);
 router.get("/:id", authMiddleware, postController.getPostById);
-
-// Define HTTP routes for post creation, update, and deletion
 router.post("/", authMiddleware, multerMiddleware, postController.createPost);
-router.put("/:id", authMiddleware, postController.updatePostById);
-router.delete("/:id", authMiddleware, postController.deletePostById);
 
 // Export router
 module.exports = router;
